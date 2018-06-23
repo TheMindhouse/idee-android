e8cf76067a5c9777f784a1d21daae2fc5702edb4
kmisztal <krmisztal@gmail.com>
2018-06-23 17:49:22 +0200

    Improved styling

--- app/src/main/java/io/mindhouse/idee/ui/auth/AuthActivity.kt
+++ app/src/main/java/io/mindhouse/idee/ui/auth/AuthActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.transition.TransitionManager
 import android.view.View
+import android.view.WindowManager
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
@@ -34,6 +35,7 @@ class AuthActivity : BaseActivity<AuthViewState, AuthViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         initFbLogin()
         initGoogleLogin()
