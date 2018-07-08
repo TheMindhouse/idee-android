9ec900beafbaf2cc356c6bb346f0484ad45c5e74
kmisztal <krmisztal@gmail.com>
2018-07-08 10:20:58 +0200

    Improved my account layout

--- app/src/main/java/io/mindhouse/idee/App.kt
+++ app/src/main/java/io/mindhouse/idee/App.kt
@@ -10,6 +10,7 @@ import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import io.fabric.sdk.android.Fabric
+import io.mindhouse.idee.di.component.DaggerAppComponent
 import timber.log.Timber
 import javax.inject.Inject
 
